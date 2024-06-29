40c78bfee99b57bc922afb409364822ce5c8a3bb
Gia Huy Tran <tonytran0314@gmail.com>
2024-06-29 12:02:50 -0400

    feat: watch video layout

---
+++ src/components/video/Video.vue
@@ -0,0 +1,73 @@
+<script setup>
+    
+</script>
+
+<template>
+    <div id="video-container" class="container">
+        <div id="video">
+            <video controls>
+                <source src="http://127.0.0.1:8000/storage/videoplayback.mp4" type="video/mp4">
+            </video>
+            <!-- <div id="reaction">
+                <div id="my-reaction"></div>
+                <div id="reactions"></div>
+            </div> -->
+        </div>
+        <div id="video-detail">
+            <h1>Title here</h1>
+            <p>00/00/2024</p>
+            <p>Description</p>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    @import '/src/assets/variables';
+
+    #video-container {
+        display: flex;
+        flex-direction: column;
+        gap: 3.5rem;
+    }
+
+    #video {
+        position: relative;
+        width: 100%;
+
+        video {
+            width: 100%;
+            border-radius: 1rem;
+            background-color: $darkLabel;
+        }
+
+        #reaction {
+            display: flex;
+            gap: 1rem;
+            position: absolute;
+            left: 50%;
+            transform: translateX(-50%);
+
+            #my-reaction {
+                width: 3rem;
+                height: 3rem;
+                border-radius: 50%;
+                cursor: pointer;
+                background-color: $boxColor;
+            }
+            #reactions {
+                width: 12rem;
+                height: 3rem;
+                border-radius: 2rem;
+                background-color: $boxColor;
+            }
+        }
+    }
+
+    #video-detail {
+        padding: 1rem 1.5rem;
+        border-radius: 1rem;
+        background-color: $boxColor;
+        h1 {
+        }
+    }
+</style>
--- src/views/VlogView.vue
+++ src/views/VlogView.vue
@@ -1,13 +1,57 @@
 <script setup>
-    
+    import Menu from '/src/components/partials/Menu.vue'
+    import Video from '/src/components/video/Video.vue'
+    import Feature from '/src/components/features/Feature.vue'
+    import Footer from '/src/components/partials/Footer.vue'
+    import SlidesSkeleton from '/src/components/skeletons/SlidesSkeleton.vue'
+    import FeatureSkeleton from '/src/components/skeletons/FeatureSkeleton.vue'
+
+    // an array contains the vlog categories, show features by those categories
+    const features = [
+        {
+            id: 100,
+            name: 'Buster Baumbach',
+            slug: 'buster-baumbach'
+        },
+        {
+            id: 88,
+            name: 'Nestor Lebsack',
+            slug: 'nestor-lebsack'
+        },
+    ]
 </script>
 
 <template>
-    <div>
-        Vlog view
+    <div class="master-container">
+        <Menu />
+
+        <main>
+            <Suspense>
+                <template #default>
+                    <div class="watch-video">
+                        <Video />
+                        <Feature v-for="feature in features" :feature="feature" />
+                    </div>
+                </template>
+                <template #fallback>
+                    <div class="watch-video">
+                        <SlidesSkeleton />
+                        <FeatureSkeleton />
+                    </div>
+                </template>
+            </Suspense>
+        </main>
+
+        <Footer />
     </div>
 </template>
 
-<style>
-    
+<style lang="scss" scoped>
+    @import '/src/assets/variables';
+
+    .watch-video {
+        display: flex;
+        flex-direction: column;
+        gap: 4rem;
+    }
 </style>
