f06465ed792fa80c8cb9ad281016fe6e890b1b33
Gia Huy Tran <tonytran0314@gmail.com>
2024-07-02 18:02:51 -0400

    style: error page

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -3,16 +3,37 @@
 </script>
 
 <template>
-    <div class="master-container">
-        <p>404 Not Found</p>
-        <router-link :to="{ name: 'Home' }">Back to Home</router-link>
+    <div class="center-full-height master-container">
+        <div class="error-header">
+            <h1>404</h1>
+            <h1>Page Not Found</h1>
+        </div>
+        <router-link :to="{ name: 'Home' }" class="back-home-button">
+            <h6>Back to Home</h6>
+        </router-link>
     </div>
 </template>
 
 <style lang="scss" scoped>
     @import '/src/assets/variables';
 
-    a {
+    .center-full-height {
+        height: 100dvh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .error-header {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .back-home-button {
+        padding: .75rem 1.5rem;
+        border-radius: 1.5rem;
         background-color: $red;
     }
     
