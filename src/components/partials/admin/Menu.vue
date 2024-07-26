b9bcaea9ecefe64bee1f26e298c18b80ffa31f2b
Gia Huy Tran <tonytran0314@gmail.com>
2024-07-26 04:09:43 -0400

    style: move admin menu to the bottom

--- src/components/partials/admin/Menu.vue
+++ src/components/partials/admin/Menu.vue
@@ -10,6 +10,18 @@
     <div class="bg-box h-dvh py-8 px-0 fixed w-[16rem]">
         <div class="space-y-12 pt-12">
             <div class="space-y-2">
+                <p class="text-dark-label text-sm uppercase pl-8">Client</p>
+                <nav class="space-y-8 text-white">
+                    <router-link 
+                        :to="{ name: 'Home' }"
+                        class="flex items-center gap-4 py-3 pl-12 hover:bg-my-blue"
+                    >
+                        <font-awesome-icon :icon="['fas', 'home']" />
+                        <div>Trang chủ</div>
+                    </router-link>
+                </nav>
+            </div>
+            <div class="space-y-2">
                 <p class="text-dark-label text-sm uppercase pl-8">Admin Menu</p>
                 <nav class="text-white">
                     <router-link 
@@ -20,18 +32,6 @@
                     >
                         <font-awesome-icon :icon="['fas', link.icon]" />
                         <div>{{ link.label }}</div>
-                    </router-link>
-                </nav>
-            </div>
-            <div class="space-y-2">
-                <p class="text-dark-label text-sm uppercase pl-8">Client</p>
-                <nav class="space-y-8 text-white">
-                    <router-link 
-                        :to="{ name: 'Home' }"
-                        class="flex items-center gap-4 py-3 pl-12 hover:bg-my-blue"
-                    >
-                        <font-awesome-icon :icon="['fas', 'home']" />
-                        <div>Trang chủ</div>
                     </router-link>
                 </nav>
             </div>
