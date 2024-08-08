a5fc62aa08f9835ee5f9370559e27c02337f7ecc
Gia Huy Tran <tonytran0314@gmail.com>
2024-08-08 03:26:58 -0400

    style: dropdown on table

--- src/views/admin/CategoryManagementView.vue
+++ src/views/admin/CategoryManagementView.vue
@@ -93,14 +93,14 @@
                                     class="cursor-pointer" />
                                 <div
                                     v-if="dropdownIndex === category.id"
-                                    class="bg-gray-700 rounded absolute z-50 -left-11 top-5">
+                                    class="bg-gray-700 rounded absolute z-50 -left-16 top-5">
                                     <div 
                                         @click="modal.open(EditCategory, category)"
-                                        class="flex gap-1 items-center rounded cursor-pointer py-2 px-4 dark:hover:bg-gray-500">
+                                        class="flex gap-1 items-center rounded cursor-pointer py-3 px-6 dark:hover:bg-gray-500">
                                         <font-awesome-icon :icon="['fas', 'pen']" class="w-3 h-3 me-2" />
                                         <span>Sửa</span>
                                     </div>
-                                    <div class="flex gap-1 items-center cursor-pointer rounded py-2 px-4 dark:hover:bg-gray-600">
+                                    <div class="flex gap-1 items-center cursor-pointer rounded py-3 px-6 dark:hover:bg-gray-600">
                                         <font-awesome-icon :icon="['fas', 'trash']" class="w-3 h-3 me-2" />
                                         <span>Xoá</span>
                                     </div>
