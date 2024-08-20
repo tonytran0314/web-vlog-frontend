db162ff43ef3e84d4e116d55153d2bc731f7089a
Gia Huy Tran <tonytran0314@gmail.com>
2024-08-20 03:20:45 -0400

    feat: install video player

--- src/components/video/Video.vue
+++ src/components/video/Video.vue
@@ -1,6 +1,7 @@
 <script setup>
     import { storeToRefs }          from 'pinia'
     import { useVlogStore }         from '@/stores/vlogs'
+    import { Player, Video, DefaultUi, PipControl } from '@vime/vue-next'
     // import { onMounted, ref }       from 'vue'
     // import { useVlogControlsStore } from '@/stores/vlog-controls'
 
@@ -12,10 +13,13 @@
     // onMounted(() => {
     //     controls.setVideoElement(video.value)
     // })
+
+    // To enable customized video controls, uncomment the commented codes
 </script>
 
 <template>
 
+    <!-- customized controls -->
     <!-- <video 
         ref="video" 
         @click="controls.togglePlay"
@@ -27,8 +31,17 @@
 
     </video> -->
 
-    <video class="size-full object-contain rounded-2xl" autoplay controls>
-        <source :src="vlog.video"> 
-    </video>
-    
+    <!-- default controls -->
+    <!-- <video class="size-full object-contain rounded-2xl" autoplay controls>
+        <source src="https://media.vimejs.com/720p.mp4"> 
+    </video> -->
+
+    <Player>
+        <Video crossorigin="">
+            <source  
+                :src="vlog.video" 
+                type="video/mp4" />
+        </Video>
+        <DefaultUi></DefaultUi>
+    </Player>
 </template>
