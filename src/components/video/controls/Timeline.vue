0930746af593df702f99f06fb4559becfbf5c0d4
Gia Huy Tran <tonytran0314@gmail.com>
2024-08-23 01:28:52 -0400

    disable: preview image

--- src/components/video/controls/Timeline.vue
+++ src/components/video/controls/Timeline.vue
@@ -19,11 +19,21 @@
                 class="absolute left-0 top-0 h-full rounded bg-main">
                 <div class="scale-0 size-4 bg-main rounded-full absolute -right-2 top-1/2 -translate-y-1/2 group-hover/timeline:scale-100"></div>
             </div>
-            <div
+            
+            <!-- with preview image -->
+            <!-- <div
                 id="preview"
                 class="hidden space-y-3 absolute -top-36 w-44 h-auto group-hover/timeline:block"
                 :style="{ left: previewPosition }">
                 <img src="@/assets/images/2.png" alt="preview image" class="rounded-xl border-2 border-white">
+                <p class="text-center">{{ controls.formattedTime(previewTime) }}</p>
+            </div> -->
+
+            <!-- without preview image -->
+            <div
+                id="preview"
+                class="hidden space-y-3 absolute -top-10 bg-overlay rounded-full w-44 h-auto group-hover/timeline:block"
+                :style="{ left: previewPosition }">
                 <p class="text-center">{{ controls.formattedTime(previewTime) }}</p>
             </div>
         </div>
