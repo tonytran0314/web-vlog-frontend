2de923d1002cd13b88b39d2efe2adc4b27dfff40
Gia Huy Tran <tonytran0314@gmail.com>
2024-09-03 03:23:24 -0400

    feat: login

---
+++ src/stores/auth.js
@@ -0,0 +1,39 @@
+import { useToast }     from "vue-toastification"
+import { reactive }     from "vue"
+import { useRouter }    from 'vue-router'
+import { defineStore }  from "pinia"
+
+import apiClient        from '@/api/apiClient' 
+
+const toast = useToast()
+
+export const useAuthStore = defineStore('auth', () => {
+    
+    const router = useRouter()
+
+    const user = reactive({
+        username: null,
+        password: null
+    })
+
+    const login = async () => {
+        const userData = new FormData()
+        userData.append('username', user.username)
+        userData.append('password', user.password)
+
+        try {
+            const res = await apiClient.post('/login', userData)
+            if (res.status === 200) {
+                localStorage.setItem('userToken', res.data.data.token)
+                router.push({ name: 'Dashboard' })
+            }
+        } catch (error) {
+            toast.error(error.response.data.message)
+        }
+    }
+
+    return {
+        user,
+        login,
+    }
+})
--- src/views/admin/AuthenticationView.vue
+++ src/views/admin/AuthenticationView.vue
@@ -1,5 +1,9 @@
 <script setup>
-    
+    import { storeToRefs }  from 'pinia'
+    import { useAuthStore } from '@/stores/auth'
+
+    const authentication = useAuthStore()
+    const { user } = storeToRefs(authentication)
 </script>
 
 <template>
@@ -10,13 +14,13 @@
                     <h1 class="text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                         Đăng nhập web vlog
                     </h1>
-                    <form class="space-y-4 md:space-y-6">
+                    <form @submit.prevent="authentication.login" class="space-y-4 md:space-y-6">
                         <div>
                             <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                                 Tên tài khoản
                                 <span class="text-main">*</span>
                             </label>
-                            <input type="text" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                            <input v-model="user.username" type="text" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
                             <small class="text-main font-bold">Username error</small>
                         </div>
                         <div>
@@ -24,7 +28,7 @@
                                 Mật khẩu
                                 <span class="text-main">*</span>
                             </label>
-                            <input type="password" name="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                            <input v-model="user.password" type="password" name="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
                             <small class="text-main font-bold">Password error</small>
                         </div>
                         <button type="submit" class="w-full text-white bg-my-blue hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg px-5 py-2.5 text-center dark:bg-my-blue dark:hover:bg-blue-600 dark:focus:ring-primary-800">Đăng nhập</button>
